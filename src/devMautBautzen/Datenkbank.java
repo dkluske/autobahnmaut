e4e0933ba7de606f4579317fa3949599f7a00bed
Andi <Andi@Andi-PC>
2019-11-11 09:43:52 +0100

    init Andi

---
+++ src/devMautBautzen/Datenkbank.java
@@ -0,0 +1,21 @@
+package devMautBautzen;
+
+public class Datenkbank {
+
+	private int treiber;
+	private int url;
+	private int user;
+	private int password;
+	private int connection;
+
+	public void establishConnection() {
+		// TODO - implement Datenkbank.establishConnection
+		throw new UnsupportedOperationException();
+	}
+
+	public void executeQuery() {
+		// TODO - implement Datenkbank.executeQuery
+		throw new UnsupportedOperationException();
+	}
+
+}
